9ce3dee0317f0c7d50e05a5ff2fc1caab6831f7c
simonebonifazi <simonbonifraz@gmail.com>
2022-12-20 10:02:05 +0100

    test main - does not work

---
+++ service/main.py
@@ -0,0 +1,60 @@
+import mysql.connector
+import json
+
+
+class Connection:
+
+    def __init__(self, **config):
+        self.connection = None
+        with open('db/config.json', 'r') as k:
+            config = json.load(k)
+        self.connection = mysql.connector.connect(**config)
+
+    def execute_query(self, query):
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+
+    def close(self):
+        if self.connection != None:
+            self.connection.close()
+
+
+class Query(Connection):
+    def get_all_actors(self):
+        query_actors = "select * from Actor"
+        Connection().execute_query(self, query_actors)
+
+    def get_all_film(self):
+        query_films = "select * from Film"
+        Connection().execute_query(self, query_films)
+
+
+class Printer(Query):
+    def print_all_actors(self):
+        result = Query().get_all_actors(self)
+        for i in result:
+            print(i)
+
+    def print_all_film(self):
+        result = Query().get_all_film(self)
+        for i in result:
+            print(i)
+
+
+printer = Printer()
+
+try:
+    test = printer.print_all_actors()
+    printer.execute_query(test)
+    results = printer.execute_query(test)
+
+    for result in results:
+        print(result)
+
+except mysql.connector.Error as errore:
+    print("Si è verificato un errore")
+    print(errore)
+finally:
+    printer.close()
+    print("connessione chiusa")
